6663abc798452488db392f47281a443b92b16120
Akhmad Aldo <akhmadaldox@gmail.com>
2026-07-13 10:14:21 +0700

    feat: add about page to sitemap configuration

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1.0,
     },
     {
+      url: 'https://akhmadaldo.my.id/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
       url: 'https://akhmadaldo.my.id/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
